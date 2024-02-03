0ea5412c62cc3ae9d1dbf519ab0caac81081b13e
Hector Zurga <hjzurga@gmail.com>
2024-02-03 17:13:48 -0400

    formatting App.js

--- src/App.js
+++ src/App.js
@@ -12,90 +12,110 @@ import YouTubePlayer from './components/YoutubePlayer'
 import './app.scss'
 
 const App = () => {
+	const state = useSelector((state) => state);
+	const { movies } = state;
+	const dispatch = useDispatch();
+	const [searchParams, setSearchParams] = useSearchParams();
+	const searchQuery = searchParams.get('search');
+	const [videoKey, setVideoKey] = useState();
+	const [isOpen, setOpen] = useState(false);
+	const navigate = useNavigate();
 
-  const state = useSelector((state) => state)
-  const { movies } = state  
-  const dispatch = useDispatch()
-  const [searchParams, setSearchParams] = useSearchParams()
-  const searchQuery = searchParams.get('search')
-  const [videoKey, setVideoKey] = useState()
-  const [isOpen, setOpen] = useState(false)
-  const navigate = useNavigate()
-  
-  const closeModal = () => setOpen(false)
-  
-  const closeCard = () => {
+	const closeModal = () => setOpen(false);
 
-  }
+	const closeCard = () => {};
 
-  const getSearchResults = (query) => {
-    if (query !== '') {
-      dispatch(fetchMovies(`${ENDPOINT_SEARCH}&query=`+query))
-      setSearchParams(createSearchParams({ search: query }))
-    } else {
-      dispatch(fetchMovies(ENDPOINT_DISCOVER))
-      setSearchParams()
-    }
-  }
+	const getSearchResults = (query) => {
+		if (query !== '') {
+			dispatch(fetchMovies(`${ENDPOINT_SEARCH}&query=` + query));
+			setSearchParams(createSearchParams({ search: query }));
+		} else {
+			dispatch(fetchMovies(ENDPOINT_DISCOVER));
+			setSearchParams();
+		}
+	};
 
-  const searchMovies = (query) => {
-    navigate('/')
-    getSearchResults(query)
-  }
+	const searchMovies = (query) => {
+		navigate('/');
+		getSearchResults(query);
+	};
 
-  const getMovies = () => {
-    if (searchQuery) {
-        dispatch(fetchMovies(`${ENDPOINT_SEARCH}&query=`+searchQuery))
-    } else {
-        dispatch(fetchMovies(ENDPOINT_DISCOVER))
-    }
-  }
+	const getMovies = () => {
+		if (searchQuery) {
+			dispatch(fetchMovies(`${ENDPOINT_SEARCH}&query=` + searchQuery));
+		} else {
+			dispatch(fetchMovies(ENDPOINT_DISCOVER));
+		}
+	};
 
-  const viewTrailer = (movie) => {
-    getMovie(movie.id)
-    if (!videoKey) setOpen(true)
-    setOpen(true)
-  }
+	const viewTrailer = (movie) => {
+		getMovie(movie.id);
+		if (!videoKey) setOpen(true);
+		setOpen(true);
+	};
 
-  const getMovie = async (id) => {
-    const URL = `${ENDPOINT}/movie/${id}?api_key=${API_KEY}&append_to_response=videos`
+	const getMovie = async (id) => {
+		const URL = `${ENDPOINT}/movie/${id}?api_key=${API_KEY}&append_to_response=videos`;
 
-    setVideoKey(null)
-    const videoData = await fetch(URL)
-      .then((response) => response.json())
+		setVideoKey(null);
+		const videoData = await fetch(URL).then((response) => response.json());
 
-    if (videoData.videos && videoData.videos.results.length) {
-      const trailer = videoData.videos.results.find(vid => vid.type === 'Trailer')
-      setVideoKey(trailer ? trailer.key : videoData.videos.results[0].key)
-    }
-  }
+		if (videoData.videos && videoData.videos.results.length) {
+			const trailer = videoData.videos.results.find(
+				(vid) => vid.type === 'Trailer'
+			);
+			setVideoKey(trailer ? trailer.key : videoData.videos.results[0].key);
+		}
+	};
 
-  useEffect(() => {
-    getMovies()
-  }, [])
+	useEffect(() => {
+		getMovies();
+	}, []);
 
-  return (
-    <div className="App">
-      <Header searchMovies={searchMovies} searchParams={searchParams} setSearchParams={setSearchParams} />
+	return (
+		<div className="App">
+			<Header
+				searchMovies={searchMovies}
+				searchParams={searchParams}
+				setSearchParams={setSearchParams}
+			/>
 
-      <div className="container">
-        {videoKey ? (
-          <YouTubePlayer
-            videoKey={videoKey}
-          />
-        ) : (
-          <div style={{padding: "30px"}}><h6>no trailer available. Try another movie</h6></div>
-        )}
+			<div className="container">
+				{videoKey ? (
+					<YouTubePlayer videoKey={videoKey} />
+				) : (
+					<div style={{ padding: '30px' }}>
+						<h6>no trailer available. Try another movie</h6>
+					</div>
+				)}
 
-        <Routes>
-          <Route path="/" element={<Movies movies={movies} viewTrailer={viewTrailer} closeCard={closeCard} />} />
-          <Route path="/starred" element={<Starred viewTrailer={viewTrailer} />} />
-          <Route path="/watch-later" element={<WatchLater viewTrailer={viewTrailer} />} />
-          <Route path="*" element={<h1 className="not-found">Page Not Found</h1>} />
-        </Routes>
-      </div>
-    </div>
-  )
-}
+				<Routes>
+					<Route
+						path="/"
+						element={
+							<Movies
+								movies={movies}
+								viewTrailer={viewTrailer}
+								closeCard={closeCard}
+							/>
+						}
+					/>
+					<Route
+						path="/starred"
+						element={<Starred viewTrailer={viewTrailer} />}
+					/>
+					<Route
+						path="/watch-later"
+						element={<WatchLater viewTrailer={viewTrailer} />}
+					/>
+					<Route
+						path="*"
+						element={<h1 className="not-found">Page Not Found</h1>}
+					/>
+				</Routes>
+			</div>
+		</div>
+	);
+};
 
 export default App
