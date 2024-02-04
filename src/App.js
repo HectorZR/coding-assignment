6a1c5e834a7f91c73128c9c283417dc03aee99d6
Hector Zurga <hjzurga@gmail.com>
2024-02-03 20:58:24 -0400

    refac: moved components loaded from routes to pages folder

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
-import { useEffect, useState } from 'react'
-import { Routes, Route, createSearchParams, useSearchParams, useNavigate } from "react-router-dom"
-import { useDispatch, useSelector } from 'react-redux'
-import 'reactjs-popup/dist/index.css'
-import { fetchMovies } from './data/moviesSlice'
-import { ENDPOINT_SEARCH, ENDPOINT_DISCOVER, ENDPOINT, API_KEY } from './constants'
-import Header from './components/Header'
-import Movies from './components/Movies'
-import Starred from './components/Starred'
-import WatchLater from './components/WatchLater'
-import YouTubePlayer from './components/YoutubePlayer'
-import './app.scss'
+import { useEffect, useState } from 'react';
+import {
+	Routes,
+	Route,
+	createSearchParams,
+	useSearchParams,
+	useNavigate,
+} from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import 'reactjs-popup/dist/index.css';
+import { fetchMovies } from './data/moviesSlice';
+import {
+	ENDPOINT_SEARCH,
+	ENDPOINT_DISCOVER,
+	ENDPOINT,
+	API_KEY,
+} from './constants';
+import Header from './components/Header';
+import Movies from './pages/Movies';
+import Starred from './pages/Starred';
+import WatchLater from './pages/WatchLater';
+import YouTubePlayer from './components/YoutubePlayer';
+import './app.scss';
 
 const App = () => {
 	const state = useSelector((state) => state);
@@ -118,4 +129,4 @@ const App = () => {
 	);
 };
 
-export default App
+export default App;
